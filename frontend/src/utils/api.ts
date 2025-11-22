1d766a489bfd00628d2404bbe63d24ce0ef9a3a6
Abhi Shekar <abhishekarpulla@gmail.com>
2025-11-23 03:10:05 +0530

    Update api.ts

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { PredictionResponse } from "@/types/detection";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const predictMicroplastics = async (file: File): Promise<PredictionResponse> => {
   const formData = new FormData();
